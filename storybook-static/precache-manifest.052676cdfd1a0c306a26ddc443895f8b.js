5573a8ebc8b4879626a6bdc60b580b69eac142cc
Michał Bednarz <mibed90@gmail.com>
2019-04-11 09:20:18 +0200

    Add files via upload

---
+++ storybook-static/precache-manifest.052676cdfd1a0c306a26ddc443895f8b.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = [
+  {
+    "revision": "9822ea8c3d474d1e3696",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.5d634768f7012c3db5f3.bundle.js"
+  },
+  {
+    "revision": "c2fed7c9a20d1e5d34bce328f7e83409",
+    "url": "static/media/right.c2fed7c9.svg"
+  },
+  {
+    "revision": "50f7c00f60268f9e97df8f9c369640e5",
+    "url": "static/media/left.50f7c00f.svg"
+  },
+  {
+    "url": "runtime~main.5d634768f7012c3db5f3.bundle.js"
+  },
+  {
+    "url": "main.5d634768f7012c3db5f3.bundle.js"
+  },
+  {
+    "revision": "4709f000cac4affe44ee775dfd281644",
+    "url": "iframe.html"
+  }
+];
