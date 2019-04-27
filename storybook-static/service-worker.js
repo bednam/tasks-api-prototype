281a2c07502935729d2c2a7495f3a5b8a1f9ad85
Michał Bednarz <mibed90@gmail.com>
2019-04-27 16:37:28 +0200

    Update storybook

--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c560aada5e68a813456babc08faaa3e8.js"
+  "precache-manifest.5b457329299c608a4c8145c77933c95f.js"
 );
 
 workbox.clientsClaim();
