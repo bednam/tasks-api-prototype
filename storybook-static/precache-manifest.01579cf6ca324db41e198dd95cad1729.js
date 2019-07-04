f945ad8dfa5a9df1e80a4e02122e0953b8aa9835
Michał Bednarz <mibed90@gmail.com>
2019-07-04 10:33:18 +0200

    update timer time interval

---
+++ storybook-static/precache-manifest.01579cf6ca324db41e198dd95cad1729.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6bf3038d0c3ead97456",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.99b57ff7bb185280a7c2.bundle.js"
+  },
+  {
+    "revision": "c2fed7c9a20d1e5d34bce328f7e83409",
+    "url": "static/media/right.c2fed7c9.svg"
+  },
+  {
+    "revision": "50f7c00f60268f9e97df8f9c369640e5",
+    "url": "static/media/left.50f7c00f.svg"
+  },
+  {
+    "url": "runtime~main.99b57ff7bb185280a7c2.bundle.js"
+  },
+  {
+    "url": "main.99b57ff7bb185280a7c2.bundle.js"
+  },
+  {
+    "revision": "029618f2cbcc8b3faacb4566028ede25",
+    "url": "iframe.html"
+  }
+];
--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.1e110eb45114c27f25f5b57c7567a56f.js"
+  "precache-manifest.01579cf6ca324db41e198dd95cad1729.js"
 );
 
 workbox.clientsClaim();
